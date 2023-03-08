a163b438dcbc6d0b8661282bd9173ceca6d579bd
Izakc <95540550+izakc-spc@users.noreply.github.com>
2023-03-08 23:04:44 +0100

    Add files via upload

---
+++ updater.bat
@@ -0,0 +1,11 @@
+@echo off
+FOR /F "delims=" %%i IN ('"powershell -File .\src\updater\getHash.ps1"') DO SET VAL=%%i
+if %VAL% EQU 0 (echo newest WIM-Editor version in directory! Starting WIM-Editor... & timeout 1 >nul /nobreak & start main.bat & exit)
+if %VAL% EQU 1 (goto upDateMain)
+
+:upDateMain
+echo There is an update available!
+echo check out [https://github.com/izakc-spc/WIM-Editor]!
+echo Update site will automatically open when leaving this script!
+pause >nul & start https://github.com/izakc-spc/WIM-Editor
+exit
