581fc33124aaade6c78025feae666e6874bed016
Izakc <95540550+izakc-spc@users.noreply.github.com>
2023-03-08 20:05:42 +0100

    File Update

--- updater/updater.ps1
+++ updater/updater.ps1
@@ -1,4 +1,4 @@
-$uri = 'https://github.com/izakc-spc/WIM-Editor/raw/main/main.bat'
+$uri = 'https://github.com/izakc-spc/WIM-Editor/blob/main/main.bat'
 $theHashIHave = Get-FileHash main.bat -Algorithm SHA256
 
 try {
@@ -6,10 +6,12 @@ try {
     $memstream = [System.IO.MemoryStream]::new($content.ToCharArray())
     $thisFileHash = Get-FileHash -InputStream $memstream -Algorithm SHA256
     if($theHashIhave.Hash -eq $thisFileHash.Hash) {
-        "0"
+        $OUTPUT = "0"
+        echo $OUTPUT
     }
     else {
-        "1"
+        $OUTPUT = "1"
+        echo $OUTPUT
     }
 }
 finally {
