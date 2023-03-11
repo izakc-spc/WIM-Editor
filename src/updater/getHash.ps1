f0147a8d4805155103a96ba22856ecc03c2a73a5
Izakc <95540550+izakc-spc@users.noreply.github.com>
2023-03-12 00:40:41 +0100

    Delete src/updater directory

--- src/updater/getHash.ps1
+++
@@ -1,5 +0,0 @@
-$wc = [System.Net.WebClient]::new()
-$URI = 'https://github.com/izakc-spc/WIM-Editor/raw/main/main.bat'
-$LocalHash = Get-FileHash .\main.bat
-$OnlineHash = Get-FileHash -InputStream ($wc.OpenRead($URI))
-if ($LocalHash.Hash -eq $OnlineHash.Hash) {$OUT = 0; Write-Output $OUT} else {$OUT = 1; Write-Output $OUT}
